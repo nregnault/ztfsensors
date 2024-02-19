3778167ac2c63fda9821945f7bfd800188829313
Nicolas Regnault <nicolas.regnault@lpnhe.in2p3.fr>
2024-02-19 09:00:09 +0100

    code cleanup + doc

--- ztfsensors/pocket.py
+++ ztfsensors/pocket.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import time
+import logging
 
 import numpy as np
 from pandas.core.arrays.period import delta_to_tick
@@ -13,6 +14,21 @@ from ._pocket import PocketModel
 
 def pocket_model_derivatives(model, pix, step=0.01):
     """model derivatives w.r.t the pixel values
+
+    For now, we use numerical derivatives. It is probably possible to do better.
+
+    Parameters
+    ----------
+    model : PocketModel
+      the pocket effect model
+    pix : array_like
+      the pixel array
+    step : float
+      numerical step
+
+    Returns
+    -------
+    jacobian matrix : array_like
     """
     N = len(pix)
     J = np.zeros((N, N))
@@ -24,8 +40,38 @@ def pocket_model_derivatives(model, pix, step=0.01):
         pix[i] -= step
     return J
 
-def correct_1d(model, pix, step=0.01):
-    """Fit the undistorted pixel values (1D version)
+
+def correct_1d(model, pix, step=0.01, n_iter=5):
+    """Reconstruct the undistorted pixel values (1D version)
+
+    The undistorted pixel values are actually parameters of a least-square fit. At each step,
+    we solve the normal equation:
+
+    .. math::
+        J^T W J (pix) = J^T W (pix-model)
+
+    where :math:`J` is the jacobian matrix of the model, :math:`W` is a weight
+    matrix and :math:`pix-model` are the residuals of the previous iteration.
+
+    To speed things up, the Hessian factorization is not recomputed and is
+    recycled at each step.
+
+    Parameters
+    ----------
+    model : PocketModel
+      the model used in the reconstruction
+    pix : array_like
+      the raw pixel array. The raw pixel array is expected to include the overscan.
+      the overscan width (30 pixels) is currently hardcoded.
+    step : float
+      derivative step
+    n_iter : int
+      number of iterations
+
+    Returns
+    -------
+    undistorted pixel array : array_like
+
     """
     default_pix_val = np.median(pix)
 
@@ -45,23 +91,54 @@ def correct_1d(model, pix, step=0.01):
     start = time.perf_counter()
     mask = np.zeros_like(current_state).astype(int)
 
-    for i in range(5):
+    for i in range(n_iter):
         res = pix - model.apply(current_state)
         delta = f.solve_LDLt(JJ.T @ res)
         delta_tot += delta
         current_state += delta
+        # we need to force the overscan to zero
         current_state[-30:] = 0.
         if i == 0:
             current_state[:2] = default_pix_val
         mask[current_state<0] = 1
         current_state[current_state<0] = default_pix_val
     stop = time.perf_counter()
-    print(f'time: {stop-start}')
+    logging.info(f'time: {stop-start}')
 
     return current_state, delta_tot, mask
 
-def correct_2d(model, pix, step=0.01):
-    """
+
+def correct_2d(model, pix, step=0.01, n_iter=4):
+    """Reconstruct the undistorted pixel values (2D version)
+
+    The undistorted pixel values are actually parameters of a least-square fit. At each step,
+    we solve the normal equation:
+
+    .. math::
+        J^T W J (pix) = J^T W (pix-model)
+
+    where :math:`J` is the jacobian matrix of the model, :math:`W` is a weight
+    matrix and :math:`pix-model` are the residuals of the previous iteration.
+
+    To speed things up, the Hessian factorization is not recomputed and is
+    recycled at each step.
+
+    Parameters
+    ----------
+    model : PocketModel
+      the model used in the reconstruction
+    pix : array_like
+      the raw pixel array. The raw pixel array is expected to include the overscan.
+      the overscan width (30 pixels) is currently hardcoded.
+    step : float
+      derivative step
+    n_iter : int
+      number of iterations
+
+    Returns
+    -------
+    undistorted pixel array : array_like
+
     """
     default_pix_val = np.median(pix)
 
@@ -82,7 +159,7 @@ def correct_2d(model, pix, step=0.01):
     delta_tot = np.zeros_like(current_state)
     mask = np.zeros_like(current_state).astype(int)
     start = time.perf_counter()
-    for i in range(4):
+    for i in range(n_iter):
         res = pix - model.apply(current_state)
         delta = f.solve_LDLt(JJ.T @ res)
         # delta = f(JJ.T @ res)
@@ -96,117 +173,3 @@ def correct_2d(model, pix, step=0.01):
     print(f'time: {stop-start}')
 
     return current_state, delta_tot, mask
-
-
-
-def fit(model, pix, step=0.01):
-    """A naive fit method.
-    """
-    # first, create a distorted image and display it
-    distorted = model.apply(pix)
-    fig, axes = pl.subplots(nrows=2, ncols=2, figsize=(12,12), sharex=True)
-    axes[0,0].plot(pix, 'k.', label='original')
-    axes[0,0].plot(distorted, 'r+:', label='distorted')
-    axes[0,0].set_title('original & distorted')
-    axes[0,0].legend()
-
-    # that, try to reconstruct the original pixels from the distorted image
-    # our starting point is precisely the distorted image
-    default_pix_val = np.median(pix)
-    start = time.perf_counter()
-    current_state = distorted.copy()
-    current_state[-30:] = 0.
-    current_state[0] = default_pix_val
-    delta_tot = np.zeros_like(current_state)
-    for i in range(4):
-        res = distorted - model.apply(current_state)
-        J = deriv(model, current_state)
-        H = J.T @ J
-        delta = np.linalg.inv(H) @ (J.T @ res)
-        delta_tot += delta
-        current_state += delta
-        current_state[-30:] = 0.
-        current_state[0:2] = default_pix_val
-    stop = time.perf_counter()
-    print(f'time: {stop-start}')
-
-    axes[0,1].plot(delta_tot, 'b.')
-    axes[0,1].set_title('correction')
-    res = distorted - model.apply(current_state)
-    axes[1,0].plot(res, 'b.')
-    axes[1,0].set_title('residuals')
-    axes[1,0].set_xlabel('pix')
-
-    axes[1,1].plot(pix, 'k.', label='original')
-    axes[1,1].plot(current_state, 'c+:', label='reconstructed')
-    axes[1,1].sharey(axes[0,0])
-    axes[1,1].set_ylim((-20, 360.))
-    axes[1,1].legend()
-    axes[1,1].set_title('original & reconstructed')
-    axes[1,1].set_xlabel('pix')
-
-
-def fast_fit(model, pix, sky, step=0.01):
-    """A tentatively faster reconstruction 
-
-       We can attempt to speed up the reconstruction by (1) recycling the
-       matrix and its factorization (2) dropping the smaller derivatives to
-       make it more sparse (2) re-implementing everything in C++
-    """
-    # first, create a distorted image and display it
-    distorted = model.apply(pix)
-    fig, axes = pl.subplots(nrows=2, ncols=2, figsize=(12,12), sharex=True)
-    axes[0,0].plot(pix, 'k.', label='original')
-    axes[0,0].plot(distorted, 'r+:', label='distorted')
-    axes[0,0].set_title('original & distorted')
-    axes[0,0].legend()
-
-    default_pix_val = np.median(pix)
-
-    J = deriv(model, sky)
-    i,j = np.meshgrid(np.arange(J.shape[0]), np.arange(J.shape[1]))
-    v = J[i.flatten(), j.flatten()]
-    idx = np.abs(v)>1.E-5
-    i,j = i.flatten(), j.flatten()
-    JJ = sparse.coo_matrix((v[idx], (i[idx], j[idx])), shape=J.shape)
-    H = JJ.T @ JJ
-    f = cholmod.cholesky(H) # , ordering_method='metis')
-    # return H, v, idx
-    # H = J.T @ J
-    # H_inv = np.linalg.inv(H)
-
-    # that, try to reconstruct the original pixels from the distorted image
-    # our starting point is precisely the distorted image
-    current_state = distorted.copy()
-    current_state[-30:] = 0.
-    current_state[0:2] = default_pix_val
-    delta_tot = np.zeros_like(current_state)
-    start = time.perf_counter()
-    for i in range(4):
-        # s = time.perf_counter()
-        res = distorted - model.apply(current_state)
-        # print(f'model eval: {time.perf_counter() - s}')
-        # s = time.perf_counter()
-        # delta = H_inv @ J.T @ res
-        delta = f.solve_LDLt(JJ.T @ res)
-        # print(f'inv: {time.perf_counter() - s}')
-        delta_tot += delta
-        current_state += delta
-        current_state[-30:] = 0.
-        # current_state[0:2] = default_pix_val
-    stop = time.perf_counter()
-    print(f'time: {stop-start}')
-
-    axes[0,1].plot(delta_tot, 'b.')
-    axes[0,1].set_title('correction')
-    axes[1,0].plot(res, 'b.')
-    axes[1,0].set_title('residuals')
-
-    axes[1,1].plot(pix, 'k.', label='original')
-    axes[1,1].plot(current_state, 'c+:', label='reconstructed')
-    axes[1,1].sharey(axes[0,0])
-    axes[1,1].set_ylim((-20, 360.))
-    axes[1,1].legend()
-    axes[1,1].set_title('original & reconstructed')
-
-    return H, JJ
