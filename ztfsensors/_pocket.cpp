ca010f96d8da22d2208887a246f3742903f440f1
Simon Conseil <s.conseil@ip2i.in2p3.fr>
2025-09-23 09:08:36 +0200

    fix some inconsistencies between backends
    - cpp: prevent pocket to be filled with more than the pixel value
    - numpy-nr: missing multiplication before clipping

--- ztfsensors/_pocket.cpp
+++ ztfsensors/_pocket.cpp
@@ -50,7 +50,11 @@ public:
         double y = n_i / _nmax;
         if(y<0.)
             return 0.;
-        return _cmax * pow(1.-x, _alpha) * pow(y, _beta);
+        double to_pocket = _cmax * pow(1.-x, _alpha) * pow(y, _beta);
+        if (to_pocket>n_i)
+          return n_i;
+        else
+          return to_pocket;
     }
 
     // apply the model to a line
